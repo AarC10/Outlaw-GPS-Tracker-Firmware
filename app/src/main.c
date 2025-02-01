5e9447c836d21c8617cd1271d88dbc8bae52d2e0
Aaron Chan <aarchan108@gmail.com>
2025-01-31 19:31:36 -0500

    Send lora over callback

--- app/src/main.c
+++ app/src/main.c
@@ -4,8 +4,10 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/drivers/gnss.h>
+#include <zephyr/drivers/lora.h>
 
 
 LOG_MODULE_REGISTER(main);
@@ -13,12 +15,15 @@ LOG_MODULE_REGISTER(main);
 GNSS_DATA_CALLBACK_DEFINE(DEVICE_DT_GET(DT_ALIAS(gnss)), gnss_data_cb);
 // GNSS_SATELLITES_CALLBACK_DEFINE(DEVICE_DT_GET(DT_ALIAS(gnss)), gnss_satellites_cb);
 
+static struct device *lora_dev = DEVICE_DT_GET(DT_ALIAS(lora));
+
 void gnss_data_cb(const struct device *dev, const struct gnss_data *data) {
     if (data->info.fix_status != GNSS_FIX_STATUS_NO_FIX) {
         LOG_INF("Fix acquired!");
-
+        lora_send_async(lora_dev, (uint8_t *) data, sizeof(*data), NULL);
     } else {
         LOG_INF("No fix acquired!");
+        lora_send_async(lora_dev, "NOFIX", 5, NULL);
     }
 }
 
