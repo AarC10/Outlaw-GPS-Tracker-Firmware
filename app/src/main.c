faf105fdb5da82ea6bacf959b9aa116aa7102358
Aaron Chan <aarchan108@gmail.com>
2025-01-31 19:31:36 -0500

    add a state machine

--- app/src/main.c
+++ app/src/main.c
@@ -8,16 +8,25 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/drivers/gnss.h>
 #include <zephyr/drivers/lora.h>
-
+#include <zephyr/smf.h>
 
 LOG_MODULE_REGISTER(main);
 
-GNSS_DATA_CALLBACK_DEFINE(DEVICE_DT_GET(DT_ALIAS(gnss)), gnss_data_cb);
-// GNSS_SATELLITES_CALLBACK_DEFINE(DEVICE_DT_GET(DT_ALIAS(gnss)), gnss_satellites_cb);
+GNSS_DATA_CALLBACK_DEFINE(DEVICE_DT_GET(DT_ALIAS(gnss)), gnss_data_callback);
 
+typedef enum {
+    TRANSMITTER,
+    RECEIVER
+} OutlawMode;
+
+static OutlawMode mode = TRANSMITTER;
 static struct device *lora_dev = DEVICE_DT_GET(DT_ALIAS(lora));
 
-void gnss_data_cb(const struct device *dev, const struct gnss_data *data) {
+static void gnss_data_callback(const struct device *dev, const struct gnss_data *data) {
+    if (mode == RECEIVER) {
+        return;
+    }
+
     if (data->info.fix_status != GNSS_FIX_STATUS_NO_FIX) {
         LOG_INF("Fix acquired!");
         lora_send_async(lora_dev, (uint8_t *) data, sizeof(*data), NULL);
@@ -27,13 +36,59 @@ void gnss_data_cb(const struct device *dev, const struct gnss_data *data) {
     }
 }
 
+static void lora_receive_callback(const struct device *dev, uint8_t *data, uint16_t size,
+                         int16_t rssi, int8_t snr, void *user_data) {
+    if (mode == TRANSMITTER) {
+        return;
+    }
 
-void gnss_satellites_cb(const struct device *dev, const struct gnss_satellite *satellites, uint16_t size) {
-    LOG_INF("Locked with %u satellites!", dev->name, size);
+    if (size == sizeof(struct gnss_data)) {
+        struct gnss_data *gnss_data = (struct gnss_data *) data;
+        LOG_INF("Received GNSS data:");
+        LOG_INF("Latitude: %lld", gnss_data->nav_data.latitude);
+        LOG_INF("Longitude: %lld", gnss_data->nav_data.longitude);
+        LOG_INF("Bearing: %u", gnss_data->nav_data.bearing);
+        LOG_INF("Speed: %u", gnss_data->nav_data.speed);
+        LOG_INF("Altitude: %d", gnss_data->nav_data.altitude);
+    } else {
+        LOG_INF("Received data: %s", data);
+    }
 }
 
 
-int main(void) {
+static const struct smf_state states[];
+
+enum demo_state { transmitter, receiver};
+
+struct s_object {
+    struct smf_ctx ctx;
+} s_obj;
+
+/* State transmitter */
+static void transmitter_entry(void *o) {
 
 }
 
+static void receiver_entry(void *o) {
+}
+
+
+static const struct smf_state states[] = {
+    [transmitter] = SMF_CREATE_STATE(transmitter_entry, NULL, NULL, NULL, NULL),
+    [receiver] = SMF_CREATE_STATE(receiver_entry, NULL, NULL, NULL, NULL),
+};
+
+int main(void)
+{
+    smf_set_initial(SMF_CTX(&s_obj), &states[transmitter]);
+
+    while (true) {
+        int32_t ret = smf_run_state(SMF_CTX(&s_obj));
+        if (ret) {
+            LOG_WRN("SMF returned non-zero status: %d", ret);
+        }
+        k_msleep(1000);
+    }
+
+    return 0;
+}
