ad039f61509b00ca279febc4e365425dbefd9653
Aaron Chan <aarchan108@gmail.com>
2025-09-19 22:04:55 -0400

    rewrite receiver code

--- receiver/src/main.c
+++ receiver/src/main.c
@@ -5,15 +5,14 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <zephyr/logging/log.h>
-#include <zephyr/drivers/gnss.h>
 #include <zephyr/drivers/lora.h>
-#include <zephyr/smf.h>
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/adc.h>
-#include <zephyr/logging/log_ctrl.h>
+#include <stdbool.h>
 
 #define NOFIX "NOFIX"
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+
 
 typedef struct {
     uint8_t node_id;
@@ -24,51 +23,11 @@ typedef struct {
     uint32_t speed;
 } lora_payload_t;
 
-static lora_payload_t payload;
-
-
 // ******************************************** //
 // *                LoRa                      * //
 // ******************************************** //
 
-static void lora_receive_callback(const struct device* dev, uint8_t* data, uint16_t size, int16_t rssi, int8_t snr,
-                                  void* user_data) {
-    const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
-
-    printk("Packet received (%d bytes | %d dBm | %d dB:", size, rssi, snr);
-
-    switch (size) {
-    case sizeof(struct gnss_data): {
-        struct gnss_data gnss_data_local;
-        memcpy(&gnss_data_local, data, sizeof(gnss_data_local));
-        printk("\tLatitude: %lld", gnss_data_local.nav_data.latitude);
-        printk("\tLongitude: %lld", gnss_data_local.nav_data.longitude);
-        printk("\tBearing: %u", gnss_data_local.nav_data.bearing);
-        printk("\tSpeed: %u", gnss_data_local.nav_data.speed);
-        printk("\tAltitude: %d", gnss_data_local.nav_data.altitude);
-        gpio_pin_toggle_dt(&led);
-
-        break;
-    }
-    case sizeof(lora_payload_t): {
-        lora_payload_t lora_payload_local;
-        memcpy(&lora_payload_local, data, sizeof(lora_payload_local));
-        printk("\tNode ID: %d", lora_payload_local.node_id);
-        printk("\tLatitude: %f", (double) lora_payload_local.latitude);
-        printk("\tLongitude: %f", (double) lora_payload_local.longitude);
-        printk("\tAltitude: %d", lora_payload_local.altitude);
-        printk("\tSatellites: %d", lora_payload_local.satellites_cnt);
-        printk("\tSpeed: %d", lora_payload_local.speed);
-        break;
-    }
-    case strlen(NOFIX):
-        printk("\tNo fix acquired!");
-        break;
-    default:
-        printk("\tReceived data: %s", data);
-        break;
-    }
-}
+static const struct device* lora_dev = DEVICE_DT_GET(DT_ALIAS(lora));
 
 static struct lora_modem_config lora_configuration = {
     .frequency = 903000000,
@@ -82,20 +41,67 @@ static struct lora_modem_config lora_configuration = {
     .public_network = false,
 };
 
+static void lora_receive_callback(const struct device* dev, uint8_t* data, uint16_t size, int16_t rssi, int8_t snr,
+                                  void* user_data) {
+    if (lora_configuration.tx) return;
+
+    printk("Packet received (%d bytes | %d dBm | %d dB:", size, rssi, snr);
+
+    switch (size) {
+    case sizeof(struct gnss_data): {
+        struct gnss_data gnss_data_local;
+        memcpy(&gnss_data_local, data, sizeof(gnss_data_local));
+        printk("\tLatitude: %lld", gnss_data_local.nav_data.latitude);
+        printk("\tLongitude: %lld", gnss_data_local.nav_data.longitude);
+        printk("\tBearing: %u", gnss_data_local.nav_data.bearing);
+        printk("\tSpeed: %u", gnss_data_local.nav_data.speed);
+        printk("\tAltitude: %d", gnss_data_local.nav_data.altitude);
+        break;
+    }
+    case sizeof(lora_payload_t): {
+        lora_payload_t lora_payload_local;
+        memcpy(&lora_payload_local, data, sizeof(lora_payload_local));
+        printk("\tNode ID: %d", lora_payload_local.node_id);
+        printk("\tLatitude: %f", lora_payload_local.latitude);
+        printk("\tLongitude: %f", lora_payload_local.longitude);
+        printk("\tAltitude: %d m", lora_payload_local.altitude);
+        printk("\tSpeed: %d cm/s", lora_payload_local.speed);
+        printk("\tSatellites: %d", lora_payload_local.satellites_cnt);
+        break;
+    }
+    case strlen(NOFIX):
+        printk("\tNo fix acquired!");
+        break;
+    default:
+        printk("\tReceived data: %s", data);
+        break;
+    }
+}
+
+static void receiver_entry() {
+    lora_configuration.tx = false;
+    lora_config(lora_dev, &lora_configuration);
+    gpio_pin_set_dt(&led, RECEIVER_LED_LEVEL);
+}
+
+static void receiver_run() {
+    lora_recv_async(lora_dev, lora_receive_callback, NULL);
+}
+
+
+// ******************************************** //
+// *                  PPS                     * //
+// ******************************************** //
 
 // ******************************************** //
 // *                  Main                    * //
 // ******************************************** //
-int main(void) {
-    const struct device* lora_dev = DEVICE_DT_GET(DT_ALIAS(lora));
-    lora_config(lora_dev, &lora_configuration);
 
+int main(void) {
+    receiver_entry();
     while (true) {
-        uint8_t buf[255];
-        int16_t rssi; int8_t snr;
-        int rc = lora_recv(lora_dev, buf, 255, K_SECONDS(10), &rssi, &snr);
-        printk("recv rc=%d\n", rc);
-        if (rc > 0) { printk("got %d bytes rssi=%d snr=%d\n", rc, rssi, snr); }
+        receiver_run();
+        k_msleep(100);
     }
 
     return 0;
