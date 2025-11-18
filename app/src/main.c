2f6d68ee7b239d15b28e2962efa6156d50900947
Aaron Chan <aarchan108@gmail.com>
2025-11-17 19:31:25 -0500

    Fix missing rsource in top level Kconfig. start moving main.c lora code to lib

--- app/src/main.c
+++ app/src/main.c
@@ -44,45 +44,7 @@ K_TIMER_DEFINE(tx_timer, tx_timer_handler, NULL);
 // *                LoRa                      * //
 // ******************************************** //
 
-static const struct device* lora_dev = DEVICE_DT_GET(DT_ALIAS(lora));
 
-static struct lora_modem_config lora_configuration = {
-    .frequency = 903000000,
-    .bandwidth = BW_125_KHZ,
-    .datarate = SF_12,
-    .coding_rate = CR_4_5,
-    .preamble_len = 8,
-    .tx_power = 20,
-    .tx = false,
-    .iq_inverted = false,
-    .public_network = false,
-};
-
-static void lora_receive_callback(const struct device* dev, uint8_t* data, uint16_t size, int16_t rssi, int8_t snr,
-                                  void* user_data) {
-    if (lora_configuration.tx) return;
-
-    LOG_INF("Packet received (%d bytes | %d dBm | %d dB:", size, rssi, snr);
-
-    switch (size) {
-    case sizeof(struct gnss_data): {
-        struct gnss_data gnss_data_local;
-        memcpy(&gnss_data_local, data, sizeof(gnss_data_local));
-        LOG_INF("\tLatitude: %lld", gnss_data_local.nav_data.latitude);
-        LOG_INF("\tLongitude: %lld", gnss_data_local.nav_data.longitude);
-        LOG_INF("\tBearing: %u", gnss_data_local.nav_data.bearing);
-        LOG_INF("\tSpeed: %u", gnss_data_local.nav_data.speed);
-        LOG_INF("\tAltitude: %d", gnss_data_local.nav_data.altitude);
-        break;
-    }
-    case strlen(NOFIX):
-        LOG_INF("\tNo fix acquired!");
-        break;
-    default:
-        LOG_INF("\tReceived data: %s", data);
-        break;
-    }
-}
 
 // ******************************************** //
 // *                GNSS                      * //
--- include/core/lora.h
+++ include/core/lora.h
@@ -1,2 +1,5 @@
 #pragma once
 
+void lora_receive_callback(const struct device* dev, uint8_t* data, uint16_t size, int16_t rssi, int8_t snr,
+                                  void* user_data);
+
