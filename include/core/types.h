b3e884d7741c66fe2820550e590a9cc692032e78
Aaron Chan <aarchan108@gmail.com>
2025-11-21 12:02:23 -0500

    Definea lora payload that is more compact

--- include/core/types.h
+++ include/core/types.h
@@ -3,14 +3,16 @@
 
 #define NOFIX "NOFIX"
 
-typedef struct {
-    uint8_t node_id;
-    float latitude;
-    float longitude;
+typedef struct __attribute__((__packed__)) {
+    int16_t latitude_scaled;
+    int16_t longitude_scaled;
     uint16_t altitude;
-    uint16_t satellites_cnt;
-    uint32_t speed;
+    uint16_t speed;
+    uint8_t satellites_cnt;
+    uint8_t fix_status;
+    uint8_t node_id;
 } lora_payload_t;
+
 
 
 #endif //TYPES_H
