9909ba25089a49f3f1dbc98fc2b4f88ff0eaaa21
Aaron Chan <aarchan108@gmail.com>
2025-11-22 17:25:18 -0500

    GNSS info getter

--- include/core/gnss.h
+++ include/core/gnss.h
@@ -2,6 +2,7 @@
 #define CORE_GNSS_H
 
 #include <stdbool.h>
+#include <stdint.h>
 
 // Forward Declares
 struct device;
@@ -12,5 +13,13 @@ void gnss_data_callback(const struct device* dev, const struct gnss_data* data);
 bool gnss_fix_acquired();
 
 void gnss_get_latest_data(struct gnss_data* out_data);
+
+int16_t gnss_get_latitude_scaled();
+
+int16_t gnss_get_longitude_scaled();
+
+uint8_t gnss_get_satellites_cnt();
+
+uint8_t gnss_get_fix_status();
 
 #endif //CORE_GNSS_H
--- lib/core/gnss.c
+++ lib/core/gnss.c
@@ -1,3 +1,4 @@
+#include "core/defs.h"
 #include "core/gnss.h"
 #include "core/lora.h"
 
@@ -25,4 +26,24 @@ bool gnss_fix_acquired() {
 
 void gnss_get_latest_data(struct gnss_data* out_data) {
     memcpy(out_data, &latest_gnss_data, sizeof(latest_gnss_data));
+}
+
+static float nanodeg_to_deg(int64_t nanodeg) {
+    return nanodeg / 10000000.0f;
+}
+
+int16_t gnss_get_latitude_scaled() {
+    return (int16_t)(nanodeg_to_deg(latest_gnss_data.nav_data.latitude) / LAT_LON_SCALING_FACTOR);
+}
+
+int16_t gnss_get_longitude_scaled() {
+    return (int16_t)(nanodeg_to_deg(latest_gnss_data.nav_data.longitude) / LAT_LON_SCALING_FACTOR);
+}
+
+uint8_t gnss_get_satellites_cnt() {
+    return latest_gnss_data.info.satellites_cnt;
+}
+
+uint8_t gnss_get_fix_status() {
+    return latest_gnss_data.info.fix_status;
 }
