3e5787560a8aba34aafb48aed16f7cec8454d904
Aaron Chan <aarchan108@gmail.com>
2025-11-22 01:39:53 -0500

    Define lora frame

--- include/core/defs.h
+++ include/core/defs.h
@@ -13,4 +13,14 @@ typedef struct __attribute__((__packed__)) {
     uint8_t node_id;
 } lora_payload_t;
 
+// TODO: Increases payload size which means we go past 7 byte limit for
+// less than 1 second TX time. need to determine if this is worth
+typedef struct __attribute__((__packed__)) {
+    uint8_t  version;        // 0x01
+    uint8_t  origin_id;      // who created the GPS info (node_id)
+    uint8_t  hop_count;      // times relayed
+    // max hops?
+    lora_payload_t payload;  // 7 bytes
+} lora_frame_t;
+
 #endif //TYPES_H
