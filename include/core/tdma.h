343b0649757f9225a8abfe856d3eb26ff7f8f37f
Aaron Chan <aarchan108@gmail.com>
2025-11-22 01:42:17 -0500

    define tdma constants

--- include/core/tdma.h
+++ include/core/tdma.h
@@ -1,6 +1,15 @@
 #ifndef CORE_TDMA_H
 #define CORE_TDMA_H
 
+#include <stdint.h>
+
+#define TDMA_FRAME_LEN_MS 10000
+#define TDMA_SLOT_LEN_MS 1100
+#define TDMA_MAX_SLOTS (TDMA_FRAME_LEN_MS / TDMA_SLOT_LEN_MS)
+#define TDMA_MAX_NODES 5
+
+
+
 void tdma_init(const uint8_t node_id);
 
 
