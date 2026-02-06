1cb1c265cf8f070323f7df2d5a8f61f48215bc3f
Aaron Chan <aarchan108@gmail.com>
2026-02-05 19:02:02 -0500

    blast headers

--- receiver/src/main.cpp
+++ receiver/src/main.cpp
@@ -6,7 +6,6 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/drivers/lora.h>
 #include <zephyr/logging/log_ctrl.h>
 
 #include "core/LoraTransceiver.h"
